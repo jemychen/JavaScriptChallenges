77599ad63194b7505491b93e2a729df2864054ee
Jemy <jemy@Jemys-MacBook-Pro.local>
2022-10-14 15:29:02 -0400

    Refactored even more for the fourth challenge

--- javascript4.js
+++ javascript4.js
@@ -27,10 +27,17 @@ function pick(arr){
 //     return {value: value, suit: suit};
 // }
 
+// function getCard() {
+//     const values = ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+//     const value = pick(values);
+//     const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
+
+//     return {value: value, suit: suit};
+// }
+
 function getCard() {
     const values = ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A'];
-    const value = pick(values);
     const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
 
-    return {value: value, suit: suit};
+    return {value: pick(values), suit: pick(suits)};
 }
