a7cbe8e016ab388cc9d259844f56e4d8aefa141a
Jemy <jemy@Jemys-MacBook-Pro.local>
2022-10-14 15:23:03 -0400

    Minor changes

--- javascript4.js
+++ javascript4.js
@@ -11,5 +11,10 @@
 
 function getCard() {
     const values = ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A'];
-    const idx = Math.floor(Math.random() * values.length);
+    const validx = Math.floor(Math.random() * values.length);
+    const value = values[idx];
+
+    const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
+    const suitIdx = Math.floor(Math.random() * suits.length)
+    const suit = suits[suitIdx]
 }
