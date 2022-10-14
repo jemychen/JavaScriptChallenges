9c44463a3d8caec5b906372e2af815c0f196ddba
Jemy <jemy@Jemys-MacBook-Pro.local>
2022-10-14 15:20:43 -0400

    .

--- javascript4.js
+++ javascript4.js
@@ -11,5 +11,5 @@
 
 function getCard() {
     const values = ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A'];
-    const idx = Math.random() * values.length;
+    const idx = Math.floor(Math.random() * values.length);
 }
