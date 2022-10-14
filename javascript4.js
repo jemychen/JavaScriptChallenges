28754899e12b96fc43cb62e504bf37a282a19851
Jemy <jemy@Jemys-MacBook-Pro.local>
2022-10-14 15:24:09 -0400

    Fixed index

--- javascript4.js
+++ javascript4.js
@@ -11,8 +11,8 @@
 
 function getCard() {
     const values = ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A'];
-    const validx = Math.floor(Math.random() * values.length);
-    const value = values[idx];
+    const valIdx = Math.floor(Math.random() * values.length);
+    const value = values[valIdx];
 
     const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
     const suitIdx = Math.floor(Math.random() * suits.length)
